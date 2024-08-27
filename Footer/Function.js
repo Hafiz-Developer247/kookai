71db8cb4e3e1de82230c63e31e368799eaf86b7f
Hafiz-Developer247 <hafizworkdo1@gmail.com>
2024-08-27 06:33:37 +0500

    Add files via upload

---
+++ Footer/Function.js
@@ -0,0 +1,25 @@
+console.log("JS Function");
+
+function nice(name) {
+    console.log("Hello", name + ", how are you");
+    console.log(name, "is a very good person");
+}
+
+nice("faheem");
+
+const newFunc = age => {
+    console.log ("My age is ", age);
+    console.log ("Rohan age is also", age);
+    console.log ("Most of my employee's age are also", age);
+}
+
+newFunc(24);
+
+
+for (let i = 0; i < 100; i++) {
+    console.log("Sorry");
+}
+
+for (let i = 1; i <= 100; i++) {
+    console.log(i + ".", "Sorry");
+}
